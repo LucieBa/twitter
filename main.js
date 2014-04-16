ddfe1333877794edeeb66267bba5eca676f46066
Marie Noiret <mnoiret@hotmail.fr>
2014-04-16 14:45:27 +0200

    1er Commit Pull Push ! Merciiiiiiiiiiiiiiiiiiiiiiiii !

--- main.js
+++ main.js
@@ -4,29 +4,38 @@ var express = require('express');
 
 var app = express();
 
-var mael = "face de quetsch";
-
-app.use(express.static(__dirname));
-
-app.get('/', function(request, result){
-	result.render('index.html.twig', {
-		mael:mael
-	});
-});
-
-app.listen(8080);
-
-
-
-/*
 var redis = require('redis');
 // sinon pour garantiadata
 var client = redis.createClient({
   host: 'pub-redis-12644.us-east-1-3.2.ec2.garantiadata.com',
   port: 12644
 }); 
-var user = 3;
-client.lpush(user+':tweets', "Je tweet");
-*/
+
+var id = 1; // Connecté en tant que Marie Noiret
+
+app.use(express.static(__dirname));
+
+app.get('/', function(request, result){
+	result.render('index.html.twig');
+	/*var name;
+	client.hgetall(id+':user',function(err,val)
+		{	
+			result.render('index.html.twig', {
+				nom:val.nom,
+				prenom:val.prenom
+			});
+		});*/
+
+});
+
+/*app.post('/publishtweet', function(request, result){
+	client.zadd(id+':tweets',new Date().getTime(),request.query.champTweet);
+});*/
+
+app.listen(8080);
+
+
+
+
 
 
